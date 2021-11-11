19cea0848cb4e9375942df455a4c54f6f449662a
grr14 <laoshi@laposte.net>
2021-11-11 18:29:35 +0100

    first

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello world!"
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
