99bd4231b2b6345ed420a0aaebb5c50da9c19ddf
grr14 <laoshi@laposte.net>
2021-11-28 20:05:09 +0100

    Added refresh endpoint

--- api/api.py
+++ api/api.py
@@ -6,7 +6,7 @@ from flask import Blueprint, jsonify, request
 from api.database.model_users import Users
 from extensions import guard
 
-from flask_praetorian.exceptions import AuthenticationError
+from flask_praetorian import auth_required, current_user
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -43,3 +43,32 @@ def login():
     ret = {'access_token': guard.encode_jwt_token(user)}
     print("ret=",ret)
     return (jsonify(ret), 200)
+
+@bp.route('/refresh', methods=['POST'])
+def refresh():
+    """
+    Refreshes an existing JWT by creating a new one that is a copy of the old
+    except that it has a refrehsed access expiration.
+    .. example::
+       $ curl http://localhost:5000/api/refresh -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    print("refresh request")
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {'access_token': new_token}
+    return ret, 200
+  
+  
+@bp.route('/protected')
+@auth_required
+def protected():
+    """
+    A protected endpoint. The auth_required decorator will require a header
+    containing a valid JWT
+    .. example::
+       $ curl http://localhost:5000/api/protected -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    print("protected")
+    return {'message': f'protected endpoint (allowed user {current_user().username})'}
