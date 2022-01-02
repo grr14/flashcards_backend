50d239afc6afc84a22b17a5ff2790bbf351ea90a
grr14 <laoshi@laposte.net>
2022-01-02 18:56:19 +0900

    Added deck deletion

--- api/deck_api.py
+++ api/deck_api.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, jsonify
 from api.database.model_deck import Deck
 from api.database.model_card import Card
+from extensions import db
 
 bp = Blueprint("deck", __name__, url_prefix="/deck")
 
 
 @bp.route("/get/<deck_id>")
 def get_deck(deck_id):
-    deck = Deck.query.filter_by(id=deck_id).first()
-    cards = Card.query.filter_by(deck_id=deck_id).all()
+    deck = db.session.query(Deck).filter_by(id=deck_id).first()
+    cards = db.session.query(Card).filter_by(deck_id=deck_id).all()
     # print(deck)
     print(cards)
     return jsonify(
@@ -27,6 +28,20 @@ def get_deck(deck_id):
 def get_all_decks(user_id):
     if not user_id:
         return {"message": "Incorrect request"}
-    decks = Deck.query.filter_by(creator_id=user_id).all()
+    decks = db.session.query(Deck).filter_by(creator_id=user_id).all()
     print(decks)
     return jsonify(count=len(decks), decks=[d.as_dict() for d in decks])
+
+
+@bp.route("/delete/<deck_id>", methods=["DELETE"])
+def delete(deck_id):
+    if not deck_id:
+        return {"message": "Incorrect message"}, 400
+
+    deck = db.session.query(Deck).filter_by(id=deck_id).first()
+    db.session.delete(deck)
+
+    cards = db.session.query(Card).filter_by(deck_id=deck_id).delete()
+    db.session.commit()
+    print(f"cards={cards}")
+    return {"message": "Deck has been deleted successfully"}
